6e18ec8032c81eafbe2e747e94745779ad2cdbcb
Bart De Meyer <bart.de.meyer@outlook.com>
2020-04-16 22:41:02 +0200

    Changes ON Automapper Init

--- Project.BLL_B/Automapper/AutoMapperInit.cs
+++ Project.BLL_B/Automapper/AutoMapperInit.cs
@@ -16,32 +16,4 @@ namespace Project.BLL.AutoMapper
             return config.CreateMapper();
         }
     }
-
-    public class ProjectAutoMapperProfile : Profile
-    {
-        public ProjectAutoMapperProfile()
-        {
-            CreateMap<DAL.Model.Vendor, BDO.Vendor>();
-            CreateMap<DAL.Model.Location, BDO.Location>()
-                .ForMember(s => s.LocationId, opt => opt.MapFrom(l => l.LocId))
-                .ForMember(d=>d.Computers, opt=>opt.Ignore());
-
-            CreateMap<DAL.Model.ProductUser, BDO.ProductUser>();
-            CreateMap<DAL.Model.ProgramUser, BDO.ProgramUser>();
-            CreateMap<DAL.Model.User, BDO.User>();
-
-            CreateMap<DAL.Model.Product, BDO.Product>();
-            CreateMap<DAL.Model.OperatingSystem, BDO.OperatingSystem>();
-            CreateMap<DAL.Model.Software, BDO.Software>();
-
-            CreateMap<DAL.Model.Computer, BDO.Computer>();
-            CreateMap<DAL.Model.VM, BDO.VM>();
-            CreateMap<DAL.Model.PM, BDO.PM>();
-
-            CreateMap<DAL.Model.Vlan, BDO.Vlan>();           
-            CreateMap<DAL.Model.NetworkInterface, BDO.NetworkInterface>()
-               .ForMember(s => s.NetworkInterfaceId, opt => opt.MapFrom(l => l.NwiId));
-            
-        }
-    }
 }
